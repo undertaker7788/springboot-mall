f9645eca4ce969022afdc49e63920353e8eab58a
HexschoolVuePujols <hexschool.vue@hotmail.com>
2026-09-26 16:14:02 +0800

    「商品功能 - 實作新增商品」

--- src/main/java/com/undertaker/springbootmall/dao/ProductDao.java
+++ src/main/java/com/undertaker/springbootmall/dao/ProductDao.java
@@ -1,8 +1,11 @@
 package com.undertaker.springbootmall.dao;
 
+import com.undertaker.springbootmall.dto.ProductRequest;
 import com.undertaker.springbootmall.model.Product;
 
 public interface ProductDao {
 
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
--- src/main/java/com/undertaker/springbootmall/service/ProductService.java
+++ src/main/java/com/undertaker/springbootmall/service/ProductService.java
@@ -1,7 +1,11 @@
 package com.undertaker.springbootmall.service;
 
+import com.undertaker.springbootmall.dto.ProductRequest;
 import com.undertaker.springbootmall.model.Product;
 
 public interface ProductService {
+
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
